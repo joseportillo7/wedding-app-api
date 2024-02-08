4b5fca0ac23a712c089bb488e396480ca8832ac2
JosePortillo <jose.portillo.samayoa@gmail.com>
2024-02-07 21:23:46 -0600

    feat: :art:First configuration

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
+app.get('/Welcome', (req, res) => {
+  res.send('Wedding API server');
+});
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
