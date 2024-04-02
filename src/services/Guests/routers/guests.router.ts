4d527dbfb8d5c555fa3f35397585e85b01ca73ed
JosePortillo <jose.portillo.samayoa@gmail.com>
2024-04-02 16:56:25 -0600

    change into routes

--- src/services/Guests/routers/guests.router.ts
+++ src/services/Guests/routers/guests.router.ts
@@ -12,7 +12,7 @@ export class GuestsRouter extends BaseRouter<GuestsController>{
         this.router.post('/upload', (req: Request, res: Response) => this.controller.uploadGuestInformation(req,res))
         this.router.get('/guest',(req: Request, res: Response) => this.controller.getGuestInformation(req,res));
         this.router.get('/guest/all', (req: Request, res: Response)=>this.controller.getGuestAll(req,res));
-        this.router.get('/guest/confirmation', (req: Request, res: Response)=>this.controller.getGuestConfirmation(req,res));
+        this.router.get('/guest/confirmation/all', (req: Request, res: Response)=>this.controller.getGuestConfirmation(req,res));
         this.router.put('/guest/:id',(req: Request, res: Response) => this.controller.updateGuestInformation(req,res))
         this.router.put('/guest/confirmation/:id',(req: Request, res: Response) => this.controller.updateGuestConfirmation(req,res))
         this.router.post('/guest',(req:Request, res: Response)=> this.controller.createGuest(req,res))
